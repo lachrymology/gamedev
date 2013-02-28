03e6a3129b4b436e546528b26bd49098bb16dbea
Fogus <mefogus@gmail.com>
2013-02-28 14:00:29 -0500

    Skeletor

---
+++ src/java/fogus/patagonia/Game.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package fogus.patagonia;
+
+/**
+ * @author mfogus
+ *
+ */
+public class Game {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
