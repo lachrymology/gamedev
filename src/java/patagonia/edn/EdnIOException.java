b53f49bfe0ea2fecc8c423db63042dc21becb140
Fogus <mefogus@gmail.com>
2013-06-24 14:46:06 -0400

    EDN except

---
+++ src/java/patagonia/edn/EdnIOException.java
@@ -0,0 +1,25 @@
+package patagonia.edn;
+
+import java.io.IOException;
+
+/**
+ * Indicates that an I/O error occurred. The cause will be an
+ * {@link IOException}.
+ */
+public class EdnIOException extends EdnException {
+    private static final long serialVersionUID = 1L;
+
+    public EdnIOException(String msg, IOException cause) {
+        super(msg, cause);
+    }
+
+    public EdnIOException(IOException cause) {
+        super(cause);
+    }
+
+    @Override
+    public IOException getCause() {
+        return (IOException) super.getCause();
+    }
+
+}
