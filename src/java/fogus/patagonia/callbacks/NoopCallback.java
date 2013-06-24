e42e7a6e2312ac641a594be5ecde0de05baa7de5
Fogus <mefogus@gmail.com>
2013-06-24 10:28:17 -0400

    Noop actually prints

--- src/java/fogus/patagonia/callbacks/NoopCallback.java
+++ src/java/fogus/patagonia/callbacks/NoopCallback.java
@@ -1,12 +1,19 @@
 package fogus.patagonia.callbacks;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 import fogus.patagonia.Callback;
+import fogus.patagonia.Util;
 
 public class NoopCallback implements Callback {
-	public void completed(InputStream body) {
-		System.out.println("Successfully completed the task.");
+	public void completed(InputStream is) {
+		try {
+			String body = Util.slurp(is);
+			System.out.println("Successfully completed the task: " + body);
+		} catch (IOException ioe) {
+			this.failed(ioe);
+		}
 	}
  
 	public void failed(Exception e) {
