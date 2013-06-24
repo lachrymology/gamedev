2ca7694a6a6ab162f3550127abce49254639b7fd
Fogus <mefogus@gmail.com>
2013-06-24 09:59:31 -0400

    Utils.java, slurp, quick and dirty version

--- src/java/fogus/patagonia/Util.java
+++ src/java/fogus/patagonia/Util.java
@@ -1,4 +1,22 @@
 package fogus.patagonia;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class Util {
+	public static String slurp(InputStream in) throws IOException {
+    	InputStreamReader is = new InputStreamReader(in);
+    	StringBuilder sb = new StringBuilder();
+    	BufferedReader br = new BufferedReader(is);
+    	String read = br.readLine();
+
+    	while(read != null) {
+    	    sb.append(read);
+    	    read = br.readLine();
+    	}
+
+    	return sb.toString();
+    }
 }
