0dc62c05a251989d1e5836065256bb160a9e82ca
Fogus <mefogus@gmail.com>
2013-06-24 10:18:42 -0400

    project.clj

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject patagonia/java.client "0.0.1"
+  :description "Nothing of note."
+  :dependencies [[org.apache.httpcomponents/httpcore "4.2.4"]
+                 [org.apache.httpcomponents/httpcore-nio "4.2.4"]]
+  :java-source-paths ["src/java"])
