2aac8c8883fb87915551185419506458def49f58
Fogus <mefogus@gmail.com>
2013-06-25 16:08:08 -0400

    Lein deps update

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject patagonia/java.client "0.0.1"
   :description "Nothing of note."
   :dependencies [[org.apache.httpcomponents/httpcore "4.2.4"]
-                 [org.apache.httpcomponents/httpcore-nio "4.2.4"]]
+                 [org.apache.httpcomponents/httpcore-nio "4.2.4"]
+                 [org.apache.httpcomponents/httpclient "4.2.4"]
+                 [commons-logging/commons-logging "1.1.3"]]
   :java-source-paths ["src/java"])
