cf976ffb860c7769076214050380221c0baf9bc4
fogus <mefogus@gmail.com>
2013-07-01 12:00:42 -0400

    JavaC options

--- project.clj
+++ project.clj
@@ -4,4 +4,7 @@
                  [org.apache.httpcomponents/httpcore-nio "4.2.4"]
                  [org.apache.httpcomponents/httpclient "4.2.4"]
                  [commons-logging/commons-logging "1.1.3"]]
-  :java-source-paths ["src/java"])
+  :java-source-paths ["src/java"]
+  :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
+  :omit-source true)
+
